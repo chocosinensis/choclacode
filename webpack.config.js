c7bedc8662328eee6c9631afef5fbaec8cdfe56f
choclacode <saqib@choclacode.eu.org>
2021-03-10 15:00:13 +0600

    Adjusting webpack mode by .env : v1.4.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: process.env.NODE_ENV,
   entry: './resources/js',
   output: {
     path: resolve(__dirname, 'public', 'js'),
