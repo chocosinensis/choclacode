12c20fb8bb3ac63b195c5688091f612708d36484
choclacode <saqib@choclacode.eu.org>
2021-03-18 08:20:09 +0600

    Made minor changes in discuss : v1.5.3

--- resources/helpers/socket/discuss.js
+++ resources/helpers/socket/discuss.js
@@ -18,7 +18,7 @@ const styleMsg = (name) => `Here you go @${name} <br>
 \\~\\~Strike\\~\\~ - ~~Strike~~
 \\\`Mono\\\` - \`Mono\``;
 const hintmsg = `Double tap a message to react to it ❤️
-Press up or down key to navigate your sent messages ✏️
+Hold shift key and press up or down key to navigate between your sent messages ✏️
 -p [ ... @user1 , @user2 ] &lt;msg&gt; <br> &nbsp;&nbsp; - send private message to mentioned users`;
 
 const getTime = (d = new Date()) => {
